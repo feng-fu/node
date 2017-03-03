19b88455a59e00b05df3dc4f296bca0f8580620a
feng-fu <fengfu2016@gmail.com>
2017-03-03 17:35:07 +0800

    add jd category get

--- lesson4/app.js
+++ lesson4/app.js
@@ -1,23 +1,49 @@
 var eventproxy = require('eventproxy')
 var superagent = require('superagent')
 var cheerio = require('cheerio')
+var express = require('express')
+var app = express()
 
 var url = require('url')
 var cnodeUrl = 'https://cnodejs.org/'
+var topicUrls = []
+var response;
 
 superagent.get(cnodeUrl).end((err, res) => {
   if (err) {
     return console.err(err);
   }
-  var topicUrls = []
+  response = res
   var $ = cheerio.load(res.text);
   $('#topic_list .topic_title').each((idx, element) => {
     var $element = $(element)
     var obj = {}
     obj.href = url.resolve(cnodeUrl, $element.attr('href'))
-    obj.author = $element.attr('author')
+    obj.title = $element.attr('title')
     topicUrls.push(obj)
   })
+})
 
-  console.log(topicUrls)
+
+var ep = new eventproxy()
+
+ep.after('topic_html', topicUrls.length, function (topics) {
+  topics = topics.map(function(topicPair) {
+    var topicUrl = topicPair[0]
+    var topicHtml = topicPair[1]
+    var $ = cheerio.load(topicHtml)
+    return ({
+      title: $('.topic_full_title').text().trim(),
+      href: topicUrl,
+      commit1: $('.reply_content').eq(0).text().trim()
+    })
+  })
+})
+
+
+
+app.get('/',(req, res) => {
+  res.send(response)
+}).listen(3000, (req, res) => {
+  console.log(`server in 3000`)
 })
---
+++ lesson4/index.js
@@ -0,0 +1,37 @@
+var express = require('express')
+var superagent = require('superagent')
+var cheerio = require('cheerio')
+var app = express()
+
+var jdUrl = 'https://www.jd.com/'
+var port = 8080
+
+var cate_menu = []
+
+superagent.get(jdUrl, (err, res) => {
+	if (err) {
+		console.error(err)
+	}
+
+	var $ = cheerio.load(res.text)
+
+	$('.JS_navCtn.cate_menu .cate_menu_item').each((index, element) => {
+		var $element = $(element)
+		var obj = {}
+		obj.clstag = $element.attr('clstag')
+		var cate_menu_item = []
+		var cate_menu_lk = $element.find('.cate_menu_lk')
+		cate_menu_lk.each((index1, ele) => {
+			var $ele = $(ele);
+			cate_menu_item.push($ele.text())
+		})
+		obj.detail = cate_menu_item
+		cate_menu.push(obj)
+	})
+})
+
+app.get('/',(req, res) => {
+	res.send(cate_menu)
+}).listen(port,(req, res) => {
+	console.log(`server in ${port}`)
+})
