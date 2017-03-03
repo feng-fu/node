142edbdfd5729fc30f71a9662af2cf6c8023f176
feng-fu <fengfu2016@gmail.com>
2017-03-03 18:15:52 +0800

    add test

---
+++ lesson4/test.js
@@ -0,0 +1,34 @@
+var express = require('express')
+var cheerio = require('cheerio')
+var superagent = require('superagent')
+var app = express()
+
+var shop_id = [51800,190307]
+var port = 3000
+var shop_name_arr = []
+
+
+
+var url_name = 'https://mall.jd.com/index-'
+
+var path_name = '.html'
+
+for(let i = 0;i < shop_id.length;i++){
+	let path = url_name + shop_id[i] + path_name
+	superagent.get(path, (err, res) => {
+		if (err) {
+			console.error(err)
+		}
+		var $ = cheerio.load(res.text)
+
+		var shop_name = $('.j-shopHeader')
+		console.log(shop_name)
+		shop_name_arr.push(shop_name)
+	})
+}
+
+app.get('', (req, res) => {
+	res.send(shop_name_arr)
+}).listen(port,(req, res) => {
+	console.log(`server in ${port}`)
+})
