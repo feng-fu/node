a14919b1c7f0cd301570268aa004f59f16c217cd
duocai <fengfu2016@gmail.com>
2017-03-05 23:12:34 +0800

    node learn

--- lesson3/app.js
+++ lesson3/app.js
@@ -3,33 +3,61 @@ const app = express()
 const superagent = require('superagent')
 const cheerio = require('cheerio')
 const port = 3000
+const fs = require('fs')
 
-const pathUrl = 'http://www.dy2018.com'
+const pathUrl = 'http://www.mm131.com/'
 
 app.use('/', (req,response) => {
 	superagent.get(pathUrl).end((err,res) => {
 		if (err) {
 			return next(err)
 		}
+
 		var $ = cheerio.load(res.text)
 
-		var item = [],title = []
-		$('.menu').each((idx,element) => {
+		var item = [],title = '<style>img{width: 50%;}</style>'
+		$('.new.public-box li a').each((idx,element) => {
 			var $element = $(element)
-			console.log(1)
 			item.push($element.attr('href'))
 		})
 
-		for (var i in item) {
-			superagent.get(pathUrl + item[i]).end((err,res) => {
-				$('.co_area2 co_content8 b').each((index, element) => {
+		for (let i = 0;i < item.length;i++) {
+			superagent.get(item[i]).end((err,res) => {
+				if(err){
+					console.error(err)
+				}
+				var $ = cheerio.load(res.text)
+
+				let pageArr = []
+
+				$('.content-page a').each((index, element) => {
 					let $element = $(element)
-					title.push($element.text())
+					let address = $element[0].attribs.href
+					pageArr.push(address)
+				})
+
+				//  需要消除移步问题
+
+
+
+				$('.content-pic img').each((index, element) => {
+					let $element = $(element)
+					let imgHtml = `<img src=${$element[0].attribs.src}>`
+					// title.push($element.attribs.src)
+					title += imgHtml
+					if(i === item.length - 1){
+
+						response.send(title)
+						fs.writeFile('img.html', title, (err, res) => {
+							if(err){attribs
+								return console.error(err)
+							}
+							console.log(`success write`)
+						})
+					}
 				})
 			})
 		}
-		console.log(title, item)
-		response.send(title)
 	})
 })
 
