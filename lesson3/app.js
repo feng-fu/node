6bb33a44439f1e26b04228f144d1649fb98edebb
feng-fu <fengfu2016@gmail.com>
2017-03-08 16:20:25 +0800

    add koa

--- lesson3/app.js
+++ lesson3/app.js
@@ -7,60 +7,61 @@ const fs = require('fs')
 
 const pathUrl = 'http://www.mm131.com/'
 
-app.use('/', (req,response) => {
-	superagent.get(pathUrl).end((err,res) => {
-		if (err) {
-			return next(err)
-		}
+app.use('/', (req, response) => {
+    superagent.get(pathUrl).end((err, res) => {
+        if (err) {
+            return next(err)
+        }
 
-		var $ = cheerio.load(res.text)
+        var $ = cheerio.load(res.text)
 
-		var item = [],title = '<style>img{width: 50%;}</style>'
-		$('.new.public-box li a').each((idx,element) => {
-			var $element = $(element)
-			item.push($element.attr('href'))
-		})
+        var item = [],
+            title = '<style>img{width: 50%;}</style>'
+        $('.new.public-box li a').each((idx, element) => {
+            var $element = $(element)
+            item.push($element.attr('href'))
+        })
 
-		for (let i = 0;i < item.length;i++) {
-			superagent.get(item[i]).end((err,res) => {
-				if(err){
-					console.error(err)
-				}
-				var $ = cheerio.load(res.text)
+        for (let i = 0; i < item.length; i++) {
+            superagent.get(item[i]).end((err, res) => {
+                if (err) {
+                    console.error(err)
+                }
+                var $ = cheerio.load(res.text)
 
-				let pageArr = []
+                let pageArr = []
 
-				$('.content-page a').each((index, element) => {
-					let $element = $(element)
-					let address = $element[0].attribs.href
-					pageArr.push(address)
-				})
+                $('.content-page a').each((index, element) => {
+                    let $element = $(element)
+                    let address = $element[0].attribs.href
+                    pageArr.push(address)
+                })
 
-				//  需要消除移步问题
+                //  需要消除异步问题
 
 
 
-				$('.content-pic img').each((index, element) => {
-					let $element = $(element)
-					let imgHtml = `<img src=${$element[0].attribs.src}>`
-					// title.push($element.attribs.src)
-					title += imgHtml
-					if(i === item.length - 1){
+                $('.content-pic img').each((index, element) => {
+                    let $element = $(element)
+                    let imgHtml = `<img src=${$element[0].attribs.src}>`
+                        // title.push($element.attribs.src)
+                    title += imgHtml
+                    if (i === item.length - 1) {
 
-						response.send(title)
-						fs.writeFile('img.html', title, (err, res) => {
-							if(err){attribs
-								return console.error(err)
-							}
-							console.log(`success write`)
-						})
-					}
-				})
-			})
-		}
-	})
+                        response.send(title)
+                        fs.writeFile('img.html', title, (err, res) => {
+                            if (err) {
+                                return console.error(err)
+                            }
+                            console.log(`success write`)
+                        })
+                    }
+                })
+            })
+        }
+    })
 })
 
-app.listen(port,(req, res) => {
-	console.log(`server in ${port}`)
+app.listen(port, (req, res) => {
+    console.log(`server in ${port}`)
 })
